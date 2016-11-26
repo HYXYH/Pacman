5a86e91370a3ae0987a89161158791eb33771e75
Pomis <pomis172@gmail.com>
2016-11-26 15:17:11 +0200

    kek

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,30 +5,11 @@ using System;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
-    private static System.Timers.Timer aTimer;
-
+	
     // Use this for initialization
     void Start () {
-        SetTimer();
     }
 
-    private static void SetTimer()
-    {
-        // Create a timer with a two second interval.
-        aTimer = new System.Timers.Timer(2000);
-        
-        // Hook up the Elapsed event for the timer. 
-        aTimer.Elapsed += OnTimedEvent;
-       // aTimer.AutoReset = true;
-        aTimer.Enabled = true;
-    }
-
-    private static void OnTimedEvent(object sender, ElapsedEventArgs e)
-    {
-        SceneManager.LoadScene("Pacman");
-        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.transform.position = new Vector3(0, 0, 0);
-    }
 
 
 
@@ -36,6 +17,5 @@ public class Menu : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-	
 	}
 }
